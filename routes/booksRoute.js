1cadc0b520645f751e0d97107fa318c25e29a003
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2024-12-07 10:55:41 -0800

    Added endpoint to delete a book

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -59,4 +59,22 @@ router.get('/:id', async (request, response) => {
   }
 });
 
+// Route for Delete a book
+router.delete('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
+    }
+
+    return response.status(200).send({ message: 'Book deleted successfully' });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
   export default router;
