7177347a237811a2250c7d1cac0262a8dc8e0855
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2024-12-07 11:13:39 -0800

    Re-added route to update a book

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -59,6 +59,34 @@ router.get('/:id', async (request, response) => {
   }
 });
 
+// Route for Update a Book
+router.put('/:id', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
+    }
+
+    return response.status(200).send({ message: 'Book updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // Route for Delete a book
 router.delete('/:id', async (request, response) => {
   try {
